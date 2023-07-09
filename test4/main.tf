2d4e27a66fd5cf0e8987016f2191b122989fb3be
arjun <arjunrayewar0305@gmail.com>
2023-07-09 11:53:29 +0530

    added the file

---
+++ test4/main.tf
@@ -0,0 +1,16 @@
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-069d73f3235b535bd"
+   instance_type =  var.instance_type
+
+   tags = {
+           Name = "arjuntest"
+   }
+}
