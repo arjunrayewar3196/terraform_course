a8a1e2199acec073240a4cb16fea03e7fd474d12
Arjun rayewar <107020514+arjunrayewar3196@users.noreply.github.com>
2023-07-09 11:42:01 +0530

    Update main.tf

--- test3/main.tf
+++ test3/main.tf
@@ -1,16 +1,15 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIA5V6HK7ILRGKFN6WK"
-  secret_key = "SvKn6LFKlWmghMd6p74TmrhXJn5pS64Mc99QXWX9"
+  access_key = ""
+  secret_key = ""
 }
 
+resource "aws_instance" "ec2_example" {
 
-resource "aws_instance" "webserver" {
-  ami           = "ami-069d73f3235b535bd"
-  instance_type = var.testinstances_type
+   ami           = "ami-069d73f3235b535bd"
+   instance_type =  var.instance_type
 
-  tags = {
-    Name = "server1"
-  }
+   tags = {
+           Name = var.environement_name
+   }
 }
-
